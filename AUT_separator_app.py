0143f66447fe8f3702c6a89269aaf3779bd647a1
jvanierland <69068886+jvanierland@users.noreply.github.com>
2021-02-28 15:46:17 -0600

    Update AUT_separator_app.py

--- AUT_separator_app.py
+++ AUT_separator_app.py
@@ -60,7 +60,7 @@ excel_file=st.file_uploader('Upload excel file')
 #-----------------------------------------------
 
 #Importing Data from excel sheet and replacing all 0 with numpy nans
-df=pd.read_excel(excel_file, engine='openpyxl')
+df=pd.read_csv(excel_file)
 df=df.set_index(0.00)
 df=df.replace(0, np.nan)
 
